40766738c2a64f9e857ec3fe44b299cc0aa642b6
cpp404 <70015934+cpp404@users.noreply.github.com>
2020-11-24 16:39:14 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,72 @@
+# -- coding: utf-8 --
+
+import sys
+import threading
+import msvcrt
+
+from ctypes import *
+
+sys.path.append("../MvImport")
+from MvCameraControl_class import *
+
+g_bExit = False
+
+
+# 为线程定义一个函数
+def work_thread(cam=0, pData=0, nDataSize=0):
+    stFrameInfo = MV_FRAME_OUT_INFO_EX()
+    memset(byref(stFrameInfo), 0, sizeof(stFrameInfo))
+    while True:
+        ret = cam.MV_CC_GetOneFrameTimeout(pData, nDataSize, stFrameInfo, 1000)
+        if ret == 0:
+            print("get one frame: Width[%d], Height[%d], nFrameNum[%d]" % (
+            stFrameInfo.nWidth, stFrameInfo.nHeight, stFrameInfo.nFrameNum))
+        else:
+            print("no data[0x%x]" % ret)
+        if g_bExit == True:
+            break
+
+
+if __name__ == "__main__":
+
+    deviceList = MV_CC_DEVICE_INFO_LIST()
+    tlayerType = MV_GIGE_DEVICE
+
+    # ch:枚举设备 | en:Enum device
+    ret = MvCamera.MV_CC_EnumDevices(tlayerType, deviceList)
+    if ret != 0:
+        print("enum devices fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    if deviceList.nDeviceNum == 0:
+        print("find no device!")
+        sys.exit()
+
+    print("find %d devices!" % deviceList.nDeviceNum)
+
+    for i in range(0, deviceList.nDeviceNum):
+        mvcc_dev_info = cast(deviceList.pDeviceInfo[i], POINTER(MV_CC_DEVICE_INFO)).contents
+
+        print("\ngige device: [%d]" % i)
+        strModeName = ""
+        for per in mvcc_dev_info.SpecialInfo.stGigEInfo.chModelName:
+            strModeName = strModeName + chr(per)
+        print("device model name: %s" % strModeName)
+
+        nip1 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0xff000000) >> 24)
+        nip2 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x00ff0000) >> 16)
+        nip3 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x0000ff00) >> 8)
+        nip4 = (mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x000000ff)
+        print("current ip: %d.%d.%d.%d\n" % (nip1, nip2, nip3, nip4))
+
+    nConnectionNum = input("please input the number of the device to connect:")
+
+    if int(nConnectionNum) >= deviceList.nDeviceNum:
+        print("intput error!")
+        sys.exit()
+
+    # ch:创建相机实例 | en:Creat Camera Object
+    cam = MvCamera()
+
+    # ch:选择设备并创建句柄 | en:Select device and create handle
+    stDeviceList = cast(deviceList.pDeviceInfo[int(nConnectionNum)], POINTER(MV_CC_DEVICE_INFO)).contents
